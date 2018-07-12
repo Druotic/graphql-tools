cb100ce27bd41a1acbc6228e99a71efc4dc18718
James Beavers <james.beavers@lifeomic.com>
2018-07-12 15:12:50 -0400

    Add dist/ for one-off fork

---
+++ dist/test/tests.d.ts
@@ -0,0 +1,11 @@
+import './testSchemaGenerator';
+import './testLogger';
+import './testMocking';
+import './testResolution';
+import './testMakeRemoteExecutableSchema';
+import './testMergeSchemas';
+import './testTransforms';
+import './testAlternateMergeSchemas';
+import './testErrors';
+import './testDirectives';
+import './testDelegateToSchema';
---
+++ dist/test/tests.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require('source-map-support').install();
+require("./testSchemaGenerator");
+require("./testLogger");
+require("./testMocking");
+require("./testResolution");
+require("./testMakeRemoteExecutableSchema");
+require("./testMergeSchemas");
+require("./testTransforms");
+require("./testAlternateMergeSchemas");
+require("./testErrors");
+require("./testDirectives");
+require("./testDelegateToSchema");
+//# sourceMappingURL=tests.js.map
